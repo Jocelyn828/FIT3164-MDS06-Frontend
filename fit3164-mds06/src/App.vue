de32b9c8b5545aab25d4f7b4c464fe43c363afd5
Kerouuu <kcha0154@student.monash.edu>
2025-03-16 15:42:56 +0800

    Working on Searching Page
    Add "Get perfect articles in second"

--- fit3164-mds06/src/App.vue
+++ fit3164-mds06/src/App.vue
@@ -1,47 +1,32 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import { ref } from "vue";
+
+  const searchQuery = ref("");
+
+  const performSearch = () => {
+    console.log("Searching for:", searchQuery.value);
+  };
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <h1>Get perfect articles in seconds.</h1>
+  </div>
+  <div class="flex gap-2">
+    <InputText v-model="searchQuery" placeholder="Search..." />
+    <Button label="Search" icon="pi pi-search" @click="performSearch" />
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
+  .container{
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
+  .p-inputtext {
+  width: 100%;
+  max-width: 300px;
   }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
--- fit3164-mds06/src/main.js
+++ fit3164-mds06/src/main.js
@@ -1,6 +1,25 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura';
+import Lara from '@primeuix/themes/lara';
+import Nora from '@primeuix/themes/nora';
+import Material from '@primeuix/themes/material';
+import 'primeicons/primeicons.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(PrimeVue, {
+    // Default theme configuration
+    theme: {
+        preset: Aura,
+        options: {
+            prefix: 'p',
+            darkModeSelector: '.my-app-dark',
+            cssLayer: {
+                name: 'primevue',
+                order: 'app-styles, primevue, another-css-library'
+            }
+        }
+    }
+ });
+app.mount('#app')
